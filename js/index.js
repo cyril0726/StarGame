7fa70911db1203cd6763256b427abbae906f73ee
cyril0726 <155260977+cyril0726@users.noreply.github.com>
2026-06-02 06:56:14 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -39,6 +39,10 @@ document.addEventListener("DOMContentLoaded", () => {
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 
-hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+    hamburger.textContent =
+        navLinks.classList.contains("active")
+        ? "✕"
+        : "☰";
 });
