0616a4ffcb61e9262f8294938c0c072d7543ac77
cyril0726 <155260977+cyril0726@users.noreply.github.com>
2026-06-02 06:42:03 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -34,3 +34,11 @@ document.addEventListener("DOMContentLoaded", () => {
   // INIT
   showSection("home");
 });
+
+//Hamburger
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
