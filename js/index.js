bdd42a6097acc92da35743936f48cb2ef821984a
cyril0726 <155260977+cyril0726@users.noreply.github.com>
2026-06-02 21:14:35 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -7,29 +7,35 @@ window.addEventListener("load", () => {
 
 document.addEventListener("DOMContentLoaded", () => {
 
-  // CACHE DOM (ICI et pas en haut)
-  const sections = document.querySelectorAll(".section");
-  const navItems = document.querySelectorAll("[data-section]");
+	// CACHE DOM (ICI et pas en haut)
+	const sections = document.querySelectorAll(".section");
+	const navItems = document.querySelectorAll("[data-section]");
 
-  // NAVIGATION
-  function showSection(id){
-    sections.forEach(s =>
-      s.classList.toggle("active", s.id === id)
-    );
+	// NAVIGATION
+	function showSection(id){
+		sections.forEach(s =>
+		s.classList.toggle("active", s.id === id)
+		);
 
-    navItems.forEach(btn =>
-      btn.classList.toggle("active", btn.dataset.section === id)
-    );
+		navItems.forEach(btn =>
+		btn.classList.toggle("active", btn.dataset.section === id)
+		);
 
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  }
+		window.scrollTo({ top: 0, behavior: "smooth" });
+	}
 
-  // EVENTS
-  navItems.forEach(btn => {
-    btn.addEventListener("click", () => {
-      showSection(btn.dataset.section);
-    });
-  });
+	// EVENTS
+	navItems.forEach(btn => {
+		btn.addEventListener("click", () => {
+			showSection(btn.dataset.section);
+
+			// Si le menu hamburger est ouvert, on le ferme
+			if (navLinks.classList.contains("active")) {
+			navLinks.classList.remove("active");
+			hamburger.textContent = "☰"; // remettre l'icône hamburger
+			}
+		});
+	});
 
   // INIT
   showSection("home");
